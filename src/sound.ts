789c904f46f7f19a48e9e8301c97c730a879df85
Chris Koskey <pausebreak@users.noreply.github.com>
2022-09-05 21:23:38 -0700

    avoid AudioContext was not allowed to start warning

--- src/sound.ts
+++ src/sound.ts
@@ -1,25 +1,37 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { Howl } from "howler";
 
 // https://freesound.org/people/CosmicEmbers/sounds/387479/
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const dartSoundUrl = require("url:./sound/387479_2895542-lq.mp3");
-export const dartSound = new Howl({
-  src: dartSoundUrl,
-});
-
 // https://freesound.org/people/philRacoIndie/sounds/551543/
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const errorSoundUrl = require("url:./sound/551543_8665150-lq.mp3");
-export const errorSound = new Howl({
-  src: errorSoundUrl,
+// https://freesound.org/people/MegaPenguin13/sounds/118198/
+const brokenSoundUrl = require("url:./sound/118198_2043421-lq.mp3");
+
+const sounds = () => ({
+  dart: new Howl({
+    src: dartSoundUrl,
+  }),
+
+  error: new Howl({
+    src: errorSoundUrl,
+  }),
+
+  broken: new Howl({
+    src: brokenSoundUrl,
+    sprite: {
+      start: [400, 1750],
+    },
+  }),
 });
 
-// https://freesound.org/people/MegaPenguin13/sounds/118198/
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const brokenSoundUrl = require("url:./sound/118198_2043421-lq.mp3");
-export const brokenSound = new Howl({
-  src: brokenSoundUrl,
-  sprite: {
-    start: [400, 1750],
-  },
-});
+let theSounds;
+
+// lazy initialize the sounds to avoid "The AudioContext was not allowed to start."
+export const sound = () => {
+  if (!theSounds) {
+    theSounds = sounds();
+  }
+
+  return theSounds;
+};
