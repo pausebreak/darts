131a30f715e33ef27576dd0e2ef420392437dae3
Chris Koskey <pausebreak@users.noreply.github.com>
2022-10-11 10:04:26 -0700

    add initial GameStats

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -3,6 +3,7 @@ import { render, screen } from "@testing-library/react";
 import { App } from "./App";
 import { useStore } from "../machine";
 import { resetStores } from "../../__mocks__/zustand";
+import { ohGames } from "../games";
 
 afterEach(resetStores);
 
@@ -10,6 +11,7 @@ test("shows when someone wins", () => {
   const player = { name: "me", darts: [] };
   const store = useStore.getState();
   store.addPlayer(player);
+  store.setGame(ohGames());
   store.setPlayerWon(player);
 
   render(<App />);
